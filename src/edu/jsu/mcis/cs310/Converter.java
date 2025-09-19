bae93a654d72e172cdbfbc1c18bf07033832a9a4
omarburton <omarburton@10.80.31.154>
2025-09-19 11:48:17 -0500

    Completed work on Project 1

--- src/edu/jsu/mcis/cs310/Converter.java
+++ src/edu/jsu/mcis/cs310/Converter.java
@@ -2,6 +2,9 @@ package edu.jsu.mcis.cs310;
 
 import com.github.cliftonlabs.json_simple.*;
 import com.opencsv.*;
+import com.opencsv.exceptions.CsvException;
+import java.io.*;
+import java.util.*;
 
 public class Converter {
     
@@ -78,7 +81,54 @@ public class Converter {
         
         try {
         
-            // INSERT YOUR CODE HERE
+            // Parse CSV
+            CSVReader reader = new CSVReader(new StringReader(csvString));
+            List<String[]> rows = reader.readAll();
+            
+            // Header
+            String[] headers = rows.get(0);
+            
+            // JSON root object
+            JsonObject root = new JsonObject();
+            
+            // Add column headings
+            JsonArray colHeadings = new JsonArray();
+            Collections.addAll(colHeadings, headers);
+            root.put("ColHeadings", colHeadings);
+            
+            // ProdNums + Data
+            JsonArray prodNums = new JsonArray();
+            JsonArray data = new JsonArray();
+            
+            for (int i = 1; i < rows.size(); i++) {
+                String[] row = rows.get(i);
+                
+                // ProdNum (first column)
+                prodNums.add(row[0]);
+                
+                // Data (remaining columns)
+                JsonArray rowData = new JsonArray();
+                
+                // Title
+                rowData.add(row[1]);
+                
+                // Season and Episode as integers
+                rowData.add(Integer.parseInt(row[2]));
+                rowData.add(Integer.parseInt(row[3]));
+                
+                // Remaining as strings
+                rowData.add(row[4]);
+                rowData.add(row[5]);
+                rowData.add(row[6]);
+                
+                data.add(rowData);
+            }
+            
+            root.put("ProdNums", prodNums);
+            root.put("Data", data);
+            
+            result = root.toJson();
+
             
         }
         catch (Exception e) {
@@ -96,7 +146,45 @@ public class Converter {
         
         try {
             
-            // INSERT YOUR CODE HERE
+            JsonObject root = (JsonObject) Jsoner.deserialize(jsonString);
+            
+            // Get arrays
+            JsonArray colHeadings = (JsonArray) root.get("ColHeadings");
+            JsonArray prodNums = (JsonArray) root.get("ProdNums");
+            JsonArray data = (JsonArray) root.get("Data");
+            
+            StringWriter sw = new StringWriter();
+            CSVWriter writer = new CSVWriter(sw);
+            
+            // Write header
+            writer.writeNext((String[]) colHeadings.toArray(new String[0]));
+            
+            // Write rows
+            for (int i = 0; i < prodNums.size(); i++) {
+                String prodNum = (String) prodNums.get(i);
+                JsonArray rowData = (JsonArray) data.get(i);
+                
+                String[] row = new String[colHeadings.size()];
+                row[0] = prodNum;
+                
+                for (int j = 0; j < rowData.size(); j++) {
+                    Object val = rowData.get(j);
+
+                    if (j == 2) { 
+                        row[j + 1] = String.format("%02d", Integer.valueOf(val.toString()));
+                    } 
+                    else {
+                        row[j + 1] = val.toString();
+                    }
+                }
+
+                
+                writer.writeNext(row);
+            }
+            
+            writer.close();
+            result = sw.toString();
+
             
         }
         catch (Exception e) {
